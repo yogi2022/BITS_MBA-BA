5075ce48018b57cdc9f89d5eaba1179968f32427
GONE YOGANANDA SRIHARI PRASAD <47893951+yogi2022@users.noreply.github.com>
2022-11-25 13:40:47 +0530

    PFA_LAB_Assign3
    PFA LAB Assignment 3

---
+++ R/Say_greetings.R
@@ -0,0 +1,9 @@
+#' Provides mode for a distribution vector
+#' @param name For writing any name. Basically name of the person. A String that represents the name of the person who is to be greated
+#'
+
+greet_func <- function(name)
+{
+  greeting <- paste0('Happy Christmas ', name, '!')
+  print(greeting)
+}
